249b37cb78c5488a1c8083d844d07f4bdd5255a6
enesokurt <enes.ozkurt4@gmail.com>
2021-03-04 23:53:26 +0300

    banner added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Home.css"
+
+function Home() {
+        return (
+            <div className="home">
+                <div className="home__container">
+                    <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/41/TR-hq/2020/img/Prime/XCM_Manual_ORIGIN_1259690_1329040_TR_tr_pv_launch_hero_banners_tr_tr_3347402_1500x600_1X_tr_TR._CB405452713_.jpg" alt=""/>
+                </div>
+            </div>
+        );
+}
+
+export default Home;
