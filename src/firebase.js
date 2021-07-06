5a83e351838bfe119511c7484840fe897d13badf
enesokurt <enes.ozkurt4@gmail.com>
2021-07-06 23:05:16 +0300

    add to button reducer

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBj7z-rk3Z_e-mFlt1J3zH3hdCQjqizC8g",
+    authDomain: "challenge-cbbc2.firebaseapp.com",
+    projectId: "challenge-cbbc2",
+    storageBucket: "challenge-cbbc2.appspot.com",
+    messagingSenderId: "300864914116",
+    appId: "1:300864914116:web:9f4eff44c45b8c73ae3f91",
+    measurementId: "G-S6WHX1ZBKV"
+};
