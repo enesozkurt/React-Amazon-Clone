750cf16e6d5d6a39534f13bf836c76790a3cdb35
enesokurt <enes.ozkurt4@gmail.com>
2021-07-10 12:51:42 +0300

    set_user reducer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,29 @@ import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./components/Checkout/Checkout";
 import Login from "./components/Login/Login";
+import {useEffect} from "react";
+import { auth } from "./firebase";
+import {useStateValue} from "./StateProvider";
 
 function App() {
+    const [{}, dispatch] = useStateValue();
+    useEffect(() => {
+        // will only run once when the app component loads...
+        auth.onAuthStateChanged(authUser => {
+            console.log('THE USER İS: ', authUser);
+            if (authUser) {
+                dispatch({
+                    type: 'SET_USER',
+                    user: authUser
+                })
+            } else {
+                dispatch({
+                    type: 'SET_USER',
+                    user: null
+                })
+            }
+        })
+    }, [])
   return (
       <Router>
           <div className="App">
--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -5,12 +5,13 @@ import {useStateValue} from "../../StateProvider";
 import CheckoutProduct from "./CheckoutProduct/CheckoutProduct";
 
 function Checkout ()  {
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
         return (
             <div className="checkout">
                 <div className="checkout__left">
                     <img className="checkout__ad" src="https://images-eu.ssl-images-amazon.com/images/G/41/prime/mastercards/MC_Prime_DealsPage_1500x80.jpg" alt=""/>
                     <div className="checkout__title">
+                        <h3>Hello, {user?.email}</h3>
                         <h2>Your Shopping Basket</h2>
                         {basket.map(item => (
                             <CheckoutProduct
